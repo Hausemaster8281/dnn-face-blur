2762ed59f442e2b7d88d535f493e183d51fb1ec3
Hausemaster8281 <66825701+Hausemaster8281@users.noreply.github.com>
2025-11-25 01:36:26 +0530

    Create face_anonymizer.py
    Import DNN

---
+++ face_anonymizer.py
@@ -0,0 +1,9 @@
+import cv2
+import sys
+import numpy
+
+def main():
+    # Defined paths to the model files
+    prototxt_path = "models/deploy.prototxt"
+    model_path = "models/res10_300x300_ssd_iter_140000.caffemodel"
+
