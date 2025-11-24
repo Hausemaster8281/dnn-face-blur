80fe043306230511e59b6ed03c339b9f973a23b1
Hausemaster8281 <66825701+Hausemaster8281@users.noreply.github.com>
2025-11-25 01:22:46 +0530

    Update haar_classifier_redaction.py
    Added handling for loading, videoc-apture and session control

--- haar_classifier_redaction.py
+++ haar_classifier_redaction.py
@@ -6,3 +6,16 @@ def main():
     # A pre-trained Haar Cascade classifier for face detection is loaded (xml provided by OPENCV
     cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(cascade_path)
+
+    if face_cascade.empty():
+        print("Face cascade classifier failure.")
+        sys.exit(1)
+    # Initialize video capture from the default webcam (index 0)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open video stream.")
+        sys.exit(1)
+
+    print("Face Anonymizer (Haar Cascade) started. Press 'q' to quit.")
+
